1afe498dc44c8a86ce2dec977be41d200e7263a8
MIS <amokraneakram54@gmail.com>
2023-08-26 15:42:16 +0100

    responsive 1

--- src/components/history-item.vue
+++ src/components/history-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute left-full w-5/6 h-[90%] top-6 bg-white dark:bg-slate-900 rounded-r-2xl p-4 transition-all origin-left" :class="history.show?'scale-x-100':'scale-x-0'">
+    <div class="absolute left-0 md:left-full w-5/6 h-[90%] top-6 bg-white dark:bg-slate-900 rounded-r-2xl p-4 transition-all origin-left drop-shadow-md" :class="history.show?'scale-x-100':'scale-x-0'">
         <div class="w-full h-full flex flex-col border-t-2  dark:border-slate-950">
             <div  class="w-full h-auto border-b-2 py-2 dark:border-slate-950 hover:bg-slate-100 dark:hover:bg-slate-800 cursor-pointer" v-for="h in history.history" @click.prevent="store.setOpe(h)" :key="h.equation">
                 <div class="w-full text-right text-slate-500 dark:text-slate-400"> {{ h.equation }}</div>
